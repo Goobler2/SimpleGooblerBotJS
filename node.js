f29d7ec98fa23d44ad91d492fc46a47bbb3b4ec8
WWEMGamer2 <73326807+WWEMGamer2@users.noreply.github.com>
2022-08-13 23:32:13 +1000

    Update node.js

--- node.js
+++ node.js
@@ -1,1 +1,8 @@
+// BOT PACKAGE CREATED FULLY BY ERIC! THANK HIM
+const node = require('gooblernode') // constantly a node! don't change this!
 
+//node.on('connect', () => {
+  //console.log("Package is on!")
+//}) Note to Eric: add events
+
+node("yourToken") // inits the package
